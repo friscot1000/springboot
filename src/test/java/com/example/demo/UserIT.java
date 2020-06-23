586ed05ccdc529601bc7dabd601335b4fbfb7df7
Francisco C <fcontreras@gofundme.com>
2020-06-23 09:30:21 -0700

    update class name

--- src/test/java/com/example/demo/UserIT.java
+++ src/test/java/com/example/demo/UserIT.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.example.demo.clientproxy.UserResourceV1;
 import com.example.demo.model.User;
+
+import java.util.List;
 import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +19,10 @@ import javax.ws.rs.NotFoundException;
 
 @RunWith(SpringRunner.class)//will run with spring runner
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-public class DemoApplicationTests {
+public class UserIT {
 
     @Autowired
-    private UserResourceV1 userResourceV1;
+    private UserResourceV1 userResourceV1; // in test only have reference to resource ** so you can test endpoints really work together
 
     @Test
     public void shouldInsertUser() throws Exception {
@@ -73,5 +75,33 @@ public class DemoApplicationTests {
         //then
         user = userResourceV1.fetchUser(userUid);
         assertThat(user).isEqualToComparingFieldByField(user);
+    }
+
+    @Test
+    public void shouldFetchUsersByGender() {
+        //Given - given this data
+        UUID userUid = UUID.randomUUID();
+        User user = new User(userUid, "Francisco", "Contreras", User.Gender.MALE, 22, "fcontreras@gofundme.com");
+
+        //When - I insert user
+        userResourceV1.insertNewUser(user);
+
+        List<User> females = userResourceV1.fetchUsers("female");
+
+        assertThat(females).extracting("userUid").doesNotContain(user.getUserUid());
+        assertThat(females).extracting("firstName").doesNotContain(user.getFirstName());
+        assertThat(females).extracting("lastName").doesNotContain(user.getLastName());
+        assertThat(females).extracting("gender").doesNotContain(user.getGender());
+        assertThat(females).extracting("age").doesNotContain(user.getAge());
+        assertThat(females).extracting("email").doesNotContain(user.getEmail());
+
+        List<User> males = userResourceV1.fetchUsers(User.Gender.MALE.name());
+
+        assertThat(males).extracting("userUid").contains(user.getUserUid());
+        assertThat(males).extracting("firstName").contains(user.getFirstName());
+        assertThat(males).extracting("lastName").contains(user.getLastName());
+        assertThat(males).extracting("gender").contains(user.getGender());
+        assertThat(males).extracting("age").contains(user.getAge());
+        assertThat(males).extracting("email").contains(user.getEmail());
     }
 }
